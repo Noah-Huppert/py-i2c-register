8a80e6f68009f6d338328346b4dc60a041aef217
Noah Huppert <noahhuppert@gmail.com>
2017-04-01 18:17:02 -0400

    added travis ci file and some small register segment tests

--- py_i2c_register/test_register_segment.py
+++ py_i2c_register/test_register_segment.py
@@ -12,3 +12,9 @@ class TestRegisterSegmentToBits(unittest.TestCase):
     def test_neg(self):
         with self.assertRaises(ValueError):
             RegisterSegment.to_bits(-2, 8)
+
+class TestRegisterSegmentToInt(unittest.TestCase):
+    def test_number(self):
+        self.assertEqual(RegisterSegment.to_int([0, 0, 0, 0, 1, 1, 0, 0]), 12)
+
+class TestRegisterSegmentToTwosCompInt(unittest.TestCase):
